9b1407634e6f3800352c2cf59cdd9d1da58d42d4
ferdi2005 <ferdi.traversa@gmail.com>
2021-06-19 13:27:41 +0200

    Risposta

--- app/controllers/credentials_controller.rb
+++ app/controllers/credentials_controller.rb
@@ -3,10 +3,10 @@ class CredentialsController < ApplicationController
     user = User.new(uuid: params[:uuid], device_name: params[:device_name], token: params[:token])
     if user.save
       respond_to do |format|
-        format.json { render :status => 202}
+        format.json { render :status => 202, :json => {id: user.id }}
       end
     else
-      format.json { render :status => 400}
+      format.json { render :status => 400, :json => {id: nil}}
     end
   end
 end
