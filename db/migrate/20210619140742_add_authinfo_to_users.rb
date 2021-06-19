f7f2e26d7c490daaf21fa134c00361a1569868c2
ferdi2005 <ferdi.traversa@gmail.com>
2021-06-19 16:15:18 +0200

    Abilitazione hstore

--- db/migrate/20210619140742_add_authinfo_to_users.rb
+++ db/migrate/20210619140742_add_authinfo_to_users.rb
@@ -1,5 +1,6 @@
 class AddAuthinfoToUsers < ActiveRecord::Migration[6.1]
   def change
+    ActiveRecord::Base.connection.execute("CREATE EXTENSION hstore;")
     add_column :users, :authinfo, :hstore
   end
 end
