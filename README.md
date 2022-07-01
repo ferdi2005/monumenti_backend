4df3b10a30f4fe669c9938d93c2a6b698379bf0c
Ferdinando Traversa <ferdi.traversa@gmail.com>
2022-07-02 01:07:52 +0200

    Aggiorno wiki di prova

--- README.md
+++ README.md
@@ -2,26 +2,3 @@
 
 Installare le seguenti dipendenze:
 * sudo apt-get install libexif-dev
-
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -1,6 +1,6 @@
 module AuthenticationHelper
     $oauth_consumer = OAuth::Consumer.new(ENV["CONSUMER_KEY"], ENV["CONSUMER_SECRET"], :site => "https://commons.wikimedia.org", :request_token_path => "/w/index.php?title=Special:OAuth/initiate", :authorize_path => "/wiki/Special:OAuth/authorize", :access_token_path => "/wiki/Special:OAuth/token",)
 
-    $test_oauth_consumer = OAuth::Consumer.new(ENV["TEST_CONSUMER_KEY"], ENV["TEST_CONSUMER_SECRET"], :site => "https://ferdi.host", :request_token_path => "/w/index.php?title=Special:OAuth/initiate", :authorize_path => "/wiki/Special:OAuth/authorize", :access_token_path => "/wiki/Special:OAuth/token",)
+    $test_oauth_consumer = OAuth::Consumer.new(ENV["TEST_CONSUMER_KEY"], ENV["TEST_CONSUMER_SECRET"], :site => "https://app-test.ferdinando.me", :request_token_path => "/w/index.php?title=Special:OAuth/initiate", :authorize_path => "/wiki/Special:OAuth/authorize", :access_token_path => "/wiki/Special:OAuth/token",)
     # Sempre mettere lo / davanti
 end
